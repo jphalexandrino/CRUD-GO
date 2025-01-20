eafcdd628daeb866ff2bd9c760724b890852ff8d
João Pedro Alexandrino <sonderinterprise@gmail.com>
2025-01-19 23:57:35 -0300

    Creating user controllers in `controller/`

---
+++ src/controller/createUser.go
@@ -0,0 +1,21 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jphalexandrino/CRUD-GO/src/configuration/rest_err"
+	"github.com/jphalexandrino/CRUD-GO/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBedRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+}
